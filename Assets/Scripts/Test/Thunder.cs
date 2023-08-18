fbb3a80a5a606283224eeb4874561a24851e18e0
MinseokLee <leems041625@gmail.com>
2023-08-19 01:23:32 +0900

    [Feat] : 번개 기믹 생성 중 내일 해야징

--- Assets/Scripts/Test/Thunder.cs
+++ Assets/Scripts/Test/Thunder.cs
@@ -8,11 +8,29 @@ public class Thunder : MonoBehaviour
     #endregion
 
     #region PrivateVariables
+    private float m_coolTime = 3f;
+
+    [SerializeField] private Vector3 m_defalutPos = Vector3.zero;
+    [SerializeField] private float height = 60f;
     #endregion
 
     #region PublicMethod
     #endregion
 
     #region PrivateMethod
+    private void ShowThunder()
+    {
+
+    }
+
+    private void FindPosition(Vector3 _originPos)
+    {
+        RaycastHit2D ground = Physics2D.Raycast(_originPos, Vector3.down, 100f, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (ground.collider == null)
+            return;
+
+        
+    }
     #endregion
 }
