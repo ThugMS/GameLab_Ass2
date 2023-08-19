db27a96ccd20293f22eb9813f558ef1e9346d94b
MinseokLee <leems041625@gmail.com>
2023-08-19 23:44:37 +0900

    [Feat] : 사라지는 발판 제작 완료!!

---
+++ Assets/Scripts/InGame/Stage/FadeOutGround.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutGround : MonoBehaviour
+{
+    #region PublicVariables
+    #endregion
+
+    #region PrivateVariables
+    [SerializeField] private BoxCollider2D m_collider;
+    [SerializeField] private Animator m_animator;
+    #endregion
+
+    #region PublicMethod
+    #endregion
+
+    #region PrivateMethod
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(1 << collision.gameObject.layer == 1 << LayerMask.NameToLayer("Player"))
+        {
+            m_animator.SetTrigger("Touch");
+            Debug.Log("µé¾î¿È");
+        }
+    }
+    #endregion
+}
