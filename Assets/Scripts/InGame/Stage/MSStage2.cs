ced13dcc0fe6c820838033fc8bc1257d1de7b0ef
MinseokLee <leems041625@gmail.com>
2023-08-19 19:34:41 +0900

    [Fix, Feat] : 맵 다시 재공사 시작 MSStage로 설계

---
+++ Assets/Scripts/InGame/Stage/MSStage2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MSStage2 : MonoBehaviour
+{
+    #region PublicVariables
+    #endregion
+
+    #region PrivateVariables
+    [SerializeField] private GameObject m_stagePrefab;
+    #endregion
+
+    #region PublicMethod
+    #endregion
+
+    #region PrivateMethod
+    #endregion
+}
