416cb5475f3569c144c87bbb2b34bf91953c40d0
MinseokLee <leems041625@gmail.com>
2023-08-19 12:15:31 +0900

    [Feat] : 번개 떨어질 위치와 애니메이션 추가

--- Assets/Scripts/Test/Thunder.cs
+++ Assets/Scripts/Test/Thunder.cs
@@ -9,28 +9,64 @@ public class Thunder : MonoBehaviour
 
     #region PrivateVariables
     private float m_coolTime = 3f;
+    private float m_range = 25f;
 
     [SerializeField] private Vector3 m_defalutPos = Vector3.zero;
     [SerializeField] private float height = 60f;
+    [SerializeField] private Vector3 m_thunerPos = Vector3.zero;
     #endregion
 
     #region PublicMethod
+    void Start()
+    {
+        ShowThunder();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowThunder();
+        }
+    }
     #endregion
 
     #region PrivateMethod
     private void ShowThunder()
     {
+        m_thunerPos = Vector3.zero;
 
+        for(int i = 0; i < 20; i++)
+        {
+            Vector3 randomPos = Vector3.zero;
+            randomPos.x = Random.Range(-m_range, m_range);
+            randomPos.y = height;
+
+            Debug.Log(randomPos.x);
+
+            randomPos = FindPosition(randomPos);
+
+            Debug.Log(randomPos);
+
+            if(randomPos != Vector3.zero)
+            {
+                m_thunerPos = randomPos;
+                break;
+            }
+        }
+
+        if (m_thunerPos == Vector3.zero)
+            m_thunerPos.y = height;
     }
 
-    private void FindPosition(Vector3 _originPos)
+    private Vector3 FindPosition(Vector3 _originPos)
     {
         RaycastHit2D ground = Physics2D.Raycast(_originPos, Vector3.down, 100f, 1 << LayerMask.NameToLayer("Ground"));
 
         if (ground.collider == null)
-            return;
+            return Vector3.zero;
 
-        
+        return _originPos;
     }
     #endregion
 }
